e7220d1a00ac432e23523f988fc04ee0124d15be
Eduardo <eduardo_muller0301@hotmail.com>
2020-09-18 15:55:29 -0300

    closure

---
+++ programacao_funcional/funcoes_2/closure.js
@@ -0,0 +1,10 @@
+// Closure é quand ouma função "lembra"
+//seu escopo léxico, mesmo quando a função
+//é executada fira desse escopo léxico
+
+
+const somarXMais3 = require("./closure_escopo")
+
+const x = 100
+
+console.log(somarXMais3())
