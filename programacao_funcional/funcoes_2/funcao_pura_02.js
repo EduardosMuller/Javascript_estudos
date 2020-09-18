14ac7f1c64daf6bd89bc80b5b36aa701e78efbbf
Eduardo <eduardo_muller0301@hotmail.com>
2020-09-18 09:17:33 -0300

    Função Pura 02

---
+++ programacao_funcional/funcoes_2/funcao_pura_02.js
@@ -0,0 +1,16 @@
+// Uma função pura é uma função em que o valor
+// de retornor é determinado APENAS por seus valores
+// de entrada, sem efeitos colaterais observáveis
+
+
+// impura
+function gerarNumeroAleatorio(min, max) {
+  const fator = max - min + 1
+  return parseInt(Math.random() * fator) + min
+}
+
+console.log(gerarNumeroAleatorio(1, 10))
+console.log(gerarNumeroAleatorio(1, 10))
+console.log(gerarNumeroAleatorio(1, 10))
+console.log(gerarNumeroAleatorio(1, 10))
+console.log(gerarNumeroAleatorio(1, 10))
